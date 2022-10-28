e2e16835e18e2c4fd33dee75675e3c10a189532f
sajalsardar <sajal2.stars@gmail.com>
2022-10-28 22:13:17 +0600

    new

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function($){
+
+  //banner slider 
+  var swiper = new Swiper("#banner", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+  });
+
+
+
+});
