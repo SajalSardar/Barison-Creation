46e4e59462325869682b8152b57138267dbf978d
Sajal Sardar <sajal2.stars@gmail.com>
2022-11-13 16:25:18 +0600

    add pages

--- js/script.js
+++ js/script.js
@@ -21,5 +21,20 @@ $(function($){
   //wow js 
   new WOW().init();
 
+  // menu js 
+  var main_menu = $('#main_navigation').offset().top;
+
+  $(window).on('scroll',function(){
+
+    var scroll_value = $(window).scrollTop();
+
+    if(scroll_value > main_menu){
+      $('#main_navigation').addClass('menuFix');
+    }else{
+      $('#main_navigation').removeClass('menuFix');
+    }
+
+  });
+
 
 });
